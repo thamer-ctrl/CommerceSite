6f0f41b40446d59f7f717684f599588d470519ef
thamer-ctrl <thamer.barhoumi@esprit.tn>
2021-05-08 12:19:50 +0100

    Create edit page in categoriees

--- admin/categories.php
+++ admin/categories.php
@@ -19,8 +19,53 @@ if(isset($_SESSION['username'])){
 
         // Start Manage Page
         if ($do=='Manage') {//manage page 
-            echo "welcom";
 
+            $sort ='ASC';
+            $sort_array = array('ASC','DESC');
+            if(isset($_GET['sort'])&& in_array($_GET['sort'],$sort_array) ){
+                $sort=$_GET['sort'];
+            }
+
+
+
+            $stmt2 = $con->prepare("SELECT * FROM categories ORDER BY Ordering $sort");
+            $stmt2->execute();
+            $cats = $stmt2->fetchAll();?>
+            <h1 class="text-center">Manage Categories</h1>
+            <div class="container categories">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Manage Categories
+                        <div class="ordering pull-right">
+                            Ordering:
+                            <a class="<?php if($sort=='ASC') { echo 'active';}?> " href="?sort=ASC">ASC</a>
+                            <a class="<?php if($sort=='DESC') { echo 'active';}?> "href="?sort=DESC">DESC</a>
+                        </div>
+                    </div>
+                    <div class="panel-body">
+                        <?php 
+                            foreach($cats as $cat){
+                                echo "<div class='cat'>";
+                                    echo "<div class='hidden-buttons'>";
+                                        echo "<a href='categories.php?do=Edit&catid=" . $cat['ID'] . "' class='btn btn-xs btn-primary'><i class='fa fa-edit'></i>Edit</a>";
+                                        echo "<a href='#' class='btn btn-xs btn-danger'><i class='fa fa-close'></i>Delete</a>";
+                                    echo "</div>";
+                                    echo "<h3>" . $cat['Name']."</h3>";
+                                    echo "<p>"; if($cat['Description']==''){echo 'This is empty';}else{echo $cat['Description'];}echo "</p>";
+                                    echo '<span>Ordering is ' . $cat['Ordering'] . '</span>';
+                                    if($cat['Visibility']==1){echo '<span class="visibility">Hidden</span>';}   
+                                    if($cat['Allow_Comment']==1){echo '<span class="commenting">Comment Disabled</span>';} 
+                                    if($cat['Allow_Ads']==1){echo '<span class="Advertises">Ads Disabled</span>';} 
+                                echo "</div>";
+                                echo "<hr>";
+                            }
+                        ?>
+                    </div>
+                </div>
+            
+            </div>
+
+        <?php    
         }elseif($do == 'Add'){?>
             
             
@@ -187,23 +232,53 @@ if(isset($_SESSION['username'])){
 
 
         }elseif($do ==  'Edit'){
+            //Check if Get request catid is numeric & get the integer value of it
+            $catid = isset($_GET['catid']) && is_numeric($_GET['catid']) ? intval($_GET['catid']):0;
+
+            // Select All Data Depent On This ID
+            $check = checkItem('userid','users',$userid);
+            $stmt = $con->prepare("SELECT * FROM categories WHERE ID = ? ");
+
+            // Execute Query
+            $stmt->execute(array($catid));
+
+            // Fetch The Data
+            $cat= $stmt->fetch(); // row get the data in array
+
+            // The Row Count
+            $count = $stmt->rowCount();
+
+            // If there's such ID show the form
+
+            if ($count>0) { ?>
+
+                GOOOOOOOOOOOd
+            <?php 
+
+            // If there's no shuch ID show Error Message   
+            }else{
+
+                echo "<div class='container'>";
+                $theMsg = '<div class="alert alert-danger">Theres no such ID</div>';
+                redirectHome($theMsg);
+                echo "</div>";
+            }
+
+                    }elseif($do ==  'Update'){
+                    
+                    
+                    }elseif($do ==  'Delete'){
 
 
-        }elseif($do ==  'Update'){
-        
-        
-        }elseif($do ==  'Delete'){
+                    }
 
+                    include $tp1 . 'footer.php';
 
-        }
+                }else {
+                    header('Location: index.php');
+                    exit();
+                }
+                ob_end_flush();
 
-        include $tp1 . 'footer.php';
-
-    }else {
-        header('Location: index.php');
-        exit();
-    }
-    ob_end_flush();
-
-?>
-        
+            ?>
+                    
